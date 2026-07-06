bc68178f995fa922002aa83359a93852ea55e566
hrshjha <jhaharsh451@gmail.com>
2026-07-06 23:55:25 +0530

    style: make navigation header sticky to ensure visibility during scroll

--- components/layout/NavigationLayout.tsx
+++ components/layout/NavigationLayout.tsx
@@ -5,7 +5,7 @@ import { MobileNavbar } from "@/components/navigation/MobileNavbar";
 
 export function NavigationLayout() {
   return (
-    <header className="border-b border-border-subtle/80 bg-background-page">
+    <header className="sticky top-0 z-20 border-b border-border-subtle/80 bg-background-page">
       <Container className="flex h-16 items-center justify-between gap-8">
         <Logo />
         <DesktopNavbar />
